5450fe6ebbbabb0fff3fd1625cc1d09b0d48ec28
MrRenGe <mrrenyunhong@aliyun.com>
2019-07-25 09:48:34 +0800

    字符串数学表达式计算

--- code/ES6/expressionComputation.js
+++ code/ES6/expressionComputation.js
@@ -1,3 +1,13 @@
+/**
+ * 
+ * @param {*} str 
+ * 
+ * 解析函数分装思路：
+ *       1.定义负数出现场景，将负号替换为#
+ *       2.连续和非连续数字截取保存
+ *       3.其他字符依次截取
+ */
+
 function strParse(str){
     let result = [];
     const operators = ['+','*','-','/'];
@@ -24,9 +34,9 @@ function strParse(str){
             // 如果当前为数字，并且前一个非数字那么 num = item
             // 如果当前为数字，并且下一个非数字 ，那么 num = num*10 +item; 并且把num 入栈
             // 其他则下一个是数字那么 ，num = num *10 + item ;
-            if( Number.isNaN(prev*1)){
+            if (Number.isNaN(prev*1)){
                num = parseInt(item);
-               if(Number.isNaN(next*1)){
+               if (Number.isNaN(next*1)){
                    result.push(item);
                }
             }else if(Number.isNaN(next*1)){
@@ -45,16 +55,27 @@ function strParse(str){
 }
 
 // 测试解析函数
-let str = '-(-(-212*30+15))+6/2--4+115';
-let arr = str.split('');
-console.log(strParse(arr))
+// let str = '-(-(-212*30+15))+6/2--4+115';
+// let arr = str.split('');
+// console.log(strParse(arr))
 
+/**
+ * 
+ * @param {*} arr 为传递的操作数操作符解析后的数组
+ * 
+ * 思路：
+ *      1.数字，+，—，(,# 则压入栈中，乘除运算则从栈中取左操作数，
+ *        左操作数可能为负数，因此需要再出栈一位，是#则进行 left = left*(-1)反之压入栈中还原栈
+ *        右操作数也可能是负数，因此需要判断运算符的下一位，如果为 '#' 则取下一位进行 right = right*(-1) 操作，如果为数字直接计算
+ *      2.运算表达式校验抽离函数进行检验（边计算边检验代码难以阅读，且违背封装原则）
+ *      3. 遇见,) 需要进行递归操作，因为可能会多层括号嵌套。因此括号的匹配计算需要抽离
+ * 
+ */
 
-// 计算函数
+// 注：括号计算还未抽离封装，该函数目前功能未完全实现
 function Calculation(arr){
     const stack=[];
-    const small; //保存临时弹出的 左括号
-    const operators =['+','-','(','#']
+    const operators =['+','-','(','#'];
     let result;//表示运算结果
     while(arr.length){
         let item = arr.shift();
@@ -100,9 +121,9 @@ function Calculation(arr){
         }
 
         // 遇见 ) 进行迭代弹栈计算
-        if(item==')'){
+        if (item==')'){
             let small =1;
-            while(small){
+            while (small){
                 let right = stack.pop();//右操作数
                 let negativeRight = stack.pop();//又操作数可能存在的负数负号
                 let left ,
@@ -113,7 +134,7 @@ function Calculation(arr){
                     op = stack.pop();
                     right =  parseInt(right)*(-1);
                     // 如果出现左括号，表示此次括号计算结束
-                    if(op=='('){
+                    if (op=='('){
                         small-=1;
                         result =  right
                         stack.push(result);
@@ -122,16 +143,16 @@ function Calculation(arr){
                     }else if(op=='+'||op=='-'){
                         left = stack.pop();
                         negativeLeft = stack.pop();
-                        if(negativeLeft!='#'){
+                        if (negativeLeft!='#'){
                             stack.push(negativeLeft);
                         }else{
                             left = parseInt(left)*(-1);
                         }
                         // 判断运算符进行计算
-                        if(op=='+'){
+                        if (op=='+'){
                             result = left + right;
                         }
-                        if(op=='-'){
+                        if (op=='-'){
                             result = left - right;
                         }
                     }else{
@@ -143,7 +164,7 @@ function Calculation(arr){
                     // 待实现，其他判断
                 }
             }
-
         }
     }
+    return result;
 }
